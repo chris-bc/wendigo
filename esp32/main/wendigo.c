e00a8a29fb2e3ab34f38ac926943ee3592881d61
Chris Bennetts-Cash <chris@bennettscash.id.au>
2025-04-30 14:54:43 +1000

    Create wendigo header file, add wifi, bt and ble includes

--- esp32/main/wendigo.c
+++ esp32/main/wendigo.c
@@ -4,15 +4,7 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include <stdio.h>
-#include <inttypes.h>
-#include <string.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
+#include "wendigo.h"
 
 void app_main(void)
 {
---
+++ esp32/main/wendigo.h
@@ -0,0 +1,21 @@
+#ifndef WENDIGO_H
+#define WENDIGO_H
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+#include "esp_wifi.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_gattc_api.h"
+#include "esp_gap_bt_api.h"
+
+#endif
