60d84c15a0cafcae86417ef636082dd58ea55150
chris-bc <chris@bennettscash.id.au>
2025-08-17 23:30:57 +1000

    Version bump to 0.5.1

--- Flipper/wendigo_app_i.h
+++ Flipper/wendigo_app_i.h
@@ -21,7 +21,7 @@
 
 #define IS_FLIPPER_APP           (1)
 /* TODO: Find a way to extract fap_version from application.fam */
-#define FLIPPER_WENDIGO_VERSION  "0.5.0"
+#define FLIPPER_WENDIGO_VERSION  "0.5.1"
 
 #include "wendigo_common_defs.h"
 #include "wendigo_pnl.h"
